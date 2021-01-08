e8b035239ce1a9bfcdbfbbd5812a0b64adb6095e
namanmahendra01 <namanmahendra01@gmail.com>
2021-01-08 14:45:49 +0530

    QR details,QR after scan,qr after ready

---
+++ app/src/main/java/com/androidqr/qr/QrDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.androidqr.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_details);
+    }
+}
---
+++ app/src/main/java/com/androidqr/qr/QrReady2Activity.java
@@ -0,0 +1,14 @@
+package com.androidqr.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrReady2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_ready2);
+    }
+}
