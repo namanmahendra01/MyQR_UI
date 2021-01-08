a070c4d4d49a7df044429a9296c37bdc133f0c66
namanmahendra01 <namanmahendra01@gmail.com>
2021-01-08 11:35:48 +0530

    Login Activity,Object details Activity

---
+++ app/src/main/java/com/androidqr/qr/ObjectDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.androidqr.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjectDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objec_details);
+    }
+}
